d2f2449292ef704a6920836d01fef01e935651be
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2018-11-10 16:25:31 +0200

    Working with operators: flatMap and flatMapLatest

--- RxBasics/RxOperators.playground/Contents.swift
+++ RxBasics/RxOperators.playground/Contents.swift
@@ -12,3 +12,32 @@ executeProcedure(for: "Map") {
         .dispose()
     
 }
+
+executeProcedure(for: "FlatMap and FlatMapLatest") {
+    let disposeBag = DisposeBag()
+    
+    struct GamePlayer {
+        let playerScore: Variable<Int>
+    }
+    
+    let alex = GamePlayer(playerScore: Variable(10))
+    let gemma = GamePlayer(playerScore: Variable(20))
+    
+    let currentPlayer = Variable(alex)
+    
+    currentPlayer.asObservable()
+        .flatMapLatest{ $0.playerScore.asObservable() }
+        .subscribe(onNext:{
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
+    currentPlayer.value.playerScore.value = 11
+    alex.playerScore.value = 12
+    
+    currentPlayer.value = gemma
+    
+    alex.playerScore.value = 13
+    gemma.playerScore.value = 21
+    
+}
